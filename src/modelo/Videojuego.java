b5b1f3182802819958edc816dca743162e6bffe0
Nayara <nayaretasan@gmail.com>
2025-10-03 15:44:43 +0200

    commit

--- src/modelo/Videojuego.java
+++ src/modelo/Videojuego.java
@@ -16,4 +16,50 @@ public class Videojuego {
     private int precio;
     private LocalDate fechaLanzamiento;
 
+    public Videojuego(String nombre, Genero genero, Clasificacion clasificacion, int precio, LocalDate fechaLanzamiento) {
+        this.nombre = nombre;
+        this.genero = genero;
+        this.clasificacion = clasificacion;
+        this.precio = precio;
+        this.fechaLanzamiento = fechaLanzamiento;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Genero getGenero() {
+        return genero;
+    }
+
+    public void setGenero(Genero genero) {
+        this.genero = genero;
+    }
+
+    public Clasificacion getClasificacion() {
+        return clasificacion;
+    }
+
+    public void setClasificacion(Clasificacion clasificacion) {
+        this.clasificacion = clasificacion;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+
+    public LocalDate getFechaLanzamiento() {
+        return fechaLanzamiento;
+    }
+
+    public void setFechaLanzamiento(LocalDate fechaLanzamiento) {
+        this.fechaLanzamiento = fechaLanzamiento;
+    }
 }
