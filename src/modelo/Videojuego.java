30c71388c8228000ae9bc97b7a5d225eb08cf645
Nayara <nayaretasan@gmail.com>
2025-10-03 15:40:23 +0200

    commit

--- src/modelo/Videojuego.java
+++ src/modelo/Videojuego.java
@@ -4,4 +4,16 @@ import java.time.LocalDate;
 
 public class Videojuego {
 
+    public enum Genero {
+        ACCION, ESTRATEGIA, ROL, DEPORTE, SIMULACION
+    }
+    public enum Clasificacion {
+        AAA, INDIE
+    }
+    private String nombre;
+    private Genero genero;
+    private Clasificacion clasificacion;
+    private int precio;
+    private LocalDate fechaLanzamiento;
+
 }
