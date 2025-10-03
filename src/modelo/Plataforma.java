e3f329dcc8ae3d9fb6eec48c44dd652563e9ff50
Nayara <nayaretasan@gmail.com>
2025-10-03 15:49:00 +0200

    commit

--- src/modelo/Plataforma.java
+++ src/modelo/Plataforma.java
@@ -8,4 +8,43 @@ public class Plataforma {
     private int precio;
     private int RAM; // en MiB
     private Set<Videojuego> catalogo;
+
+    public Plataforma(String nombre, int precio, int ram) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.RAM = ram;
+        this.catalogo = new HashSet<Videojuego>();
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+
+    public int getRAM() {
+        return RAM;
+    }
+
+    public void setRAM(int ram) {
+        this.RAM = ram;
+    }
+
+    public void addJuego(Videojuego v) {
+        this.catalogo.add(v);
+    }
+
+    public Set<Videojuego> getCatalogo() {
+        return this.catalogo;
+    }
 }
