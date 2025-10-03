93732b546365ec98052d4187e42e5d9ed7fcb778
Nayara <nayaretasan@gmail.com>
2025-10-03 15:44:58 +0200

    init commit

---
+++ src/modelo/Plataforma.java
@@ -0,0 +1,11 @@
+package modelo;
+
+import java.util.Set;
+
+public class Plataforma {
+
+    private String nombre;
+    private int precio;
+    private int RAM; // en MiB
+    private Set<Videojuego> catalogo;
+}
